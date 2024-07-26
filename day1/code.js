3328e891745cabd91c769691379dfcb5c2d428b3
Zeeshan Abbas <zeeshanabbasrao@gmail.com>
2024-07-26 03:03:32 -0700

    here we modify in day1

--- day1/code.js
+++ day1/code.js
@@ -1,3 +1,14 @@
 let password = prompt("enter your password");
 let newpas = password.trim();
 console.log(newpas);
+
+let msg = "mynameisjohn";
+console.log(msg.indexOf("j"));
+
+
+let str = "ilovecoding";
+console.log(str.replace("love","do"));
+
+let str2 = "mango";
+let y = str2.repeat(4);
+console.log(y);
